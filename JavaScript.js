916508970943c75a4bd7c21ce06ff4d48c1dfc1f
Nicula Dragos <110388376+AkyWellCode@users.noreply.github.com>
2022-08-03 16:33:06 +0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -13,18 +13,22 @@ function addButtons() {
 function guessButton(clicked_id) {
   var clickedButton = clicked_id;
   if(clickedButton == winnerButton) {
+    $('#removePrevText').remove();
     $('#winOrLose').append(`
-      <div class="font">
-        <p id="win" style="color:blue; font-size:35px;"> </p>
+      <div id="removePrevText">
+        <div class="font">
+        <p id="win" style="color:blue; font-size:35px;"> Congratulation, you won! </p>
+        </div>
       </div>
       `);
-    document.getElementById("win").innerHTML = "Congratulation, you won!";
   } else {
+    $('#removePrevText').remove();
     $('#winOrLose').append(`
-      <div class="font">
-        <p id="lose" style="color:red; font-size:35px;"> </p>
+      <div id="removePrevText">
+        <div class="font">
+          <p id="lose" style="color:red; font-size:35px;"> Wrong button! The winner button was: Button `+ winnerButton +`. Better luck next time! </p>
+        </div>
       </div>
       `);
-    document.getElementById("lose").innerHTML = "Wrong button! The winner button was: Button " + winnerButton + ". Better luck next time!";
   }
 }
