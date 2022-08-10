9bf290e3f9cd8c123870822a36a58342d8899193
Nicula Dragos <110388376+DragosNicula@users.noreply.github.com>
2022-08-10 18:44:29 +0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,23 @@
-var randomButton = Math.floor(Math.random() * 3) + 1, winnerButton = randomButton.toString(), winOrLose = document.getElementById("winOrLose"), text = document.createElement("text");
-function button(clicked_id) {
-  if (winnerButton === clicked_id) {
+var buttons = 1, winnerButton, text = document.createElement("text");
+
+function addButtons() {
+  var insertButtons = document.getElementById("insertButtons"), numberOfButtons = document.getElementById("numberOfButtons").value, randomButton = Math.floor(Math.random() * numberOfButtons) + 1;
+  winnerButton = randomButton.toString();
+  for (var i = 1; i <= numberOfButtons; ++i) {
+    var addButton = document.createElement("button");
+    addButton.id = buttons;
+    addButton.type = "button";
+    addButton.className = "btn btn-outline-secondary distanceButtons";
+    addButton.innerHTML = "Button " + i;
+    addButton.onclick = function() {guessButton(this.id)};
+    insertButtons.append(addButton);
+    ++buttons;
+  }
+  return false;
+}
+
+function guessButton(clicked_id) {
+  if (clicked_id === winnerButton) {
     text.innerHTML = "Congratulation, you won!";
     text.style.color = "blue";
     text.style.fontSize = "x-large";
