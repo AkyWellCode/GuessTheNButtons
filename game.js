ac98bd0ce64dc4f5bb7a9fada723980b1caf6926
Nicula Dragos <110388376+DragosNicula@users.noreply.github.com>
2022-08-15 20:40:52 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,31 @@
+let buttons = 1, winnerButton, text = document.createElement("text");
+
+function addButtons() {
+  let insertButtons = document.getElementById("insertButtons"), numberOfButtons = document.getElementById("numberOfButtons").value, randomButton = Math.floor(Math.random() * numberOfButtons) + 1;
+  winnerButton = randomButton;
+  for (let i = 1; i <= numberOfButtons; ++i) {
+    let addButton = document.createElement("button");
+    addButton.id = buttons;
+    addButton.type = "button";
+    addButton.className = "btn btn-outline-secondary distanceButtons";
+    addButton.innerHTML = "Button " + i;
+    addButton.onclick = function() {guessButton(this.id)};
+    insertButtons.append(addButton);
+    ++buttons;
+  }
+  return false;
+}
+
+function guessButton(clicked_id) {
+  if (Number(clicked_id) === winnerButton) {
+    text.innerHTML = "Congratulation, you won!";
+    text.style.color = "blue";
+    text.style.fontSize = "x-large";
+    winOrLose.append(text);
+  } else {
+    text.innerHTML = "Wrong button! The correct button is: Button " + winnerButton + ". Better luck next time!";
+    text.style.color = "red";
+    text.style.fontSize = "x-large";
+    winOrLose.append(text);
+  }
+}
